6393d442fc471490fed9d986edc4137a02d354e8
whiterabbit09 <79956778+whiterabbit09@users.noreply.github.com>
2021-11-03 17:49:42 +0900

    10주차_커밋2
    js추가

--- main.js
+++ main.js
@@ -10,3 +10,30 @@ document.addEventListener("scroll", () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+//넷바 메뉴 선택시 해당 부분으로 이동
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior : 'smooth'});
+});
+
+const contactMe = document.querySelector(".home__contact");
+contactMe.addEventListener("click", (event) => {
+    scrollIntoView("#contact");
+});
+// const contactMe = document.querySelector(".home__contact");
+// contactMe.addEventListener("click", (event) => {
+//     const target = event.target;
+//     const link = target.dataset.link;
+//     if (link == null) {
+//         return;
+//     }
+//     const scrollTo = document.querySelector(link);
+//     scrollTo.scrollIntoView({behavior : 'smooth'});
+// })
