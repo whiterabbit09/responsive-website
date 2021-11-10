ebfca0feaf143b056fc5623754a6ce6835365eae
whiterabbit09 <79956778+whiterabbit09@users.noreply.github.com>
2021-11-10 17:51:00 +0900

    11주차_커밋
    js 추가

--- main.js
+++ main.js
@@ -19,21 +19,24 @@ navbarMenu.addEventListener("click", (event) => {
     if (link == null) {
         return;
     }
+    navbarMenu.classList.remove("open");
     const scrollTo = document.querySelector(link);
     scrollTo.scrollIntoView({behavior : 'smooth'});
 });
 
+//Contact Me 버튼 클릭시 해당 부분으로 이동
 const contactMe = document.querySelector(".home__contact");
-contactMe.addEventListener("click", (event) => {
-    scrollIntoView("#contact");
+contactMe.addEventListener("click", () => {
+    document.querySelector("#contact").scrollIntoView({behavior : 'smooth'});
 });
-// const contactMe = document.querySelector(".home__contact");
-// contactMe.addEventListener("click", (event) => {
-//     const target = event.target;
-//     const link = target.dataset.link;
-//     if (link == null) {
-//         return;
-//     }
-//     const scrollTo = document.querySelector(link);
-//     scrollTo.scrollIntoView({behavior : 'smooth'});
-// })
+
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+    home.style.opacity = 1 - scrollY / homeHeight;
+});
+
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+    navbarMenu.classList.toggle("open");
+});
