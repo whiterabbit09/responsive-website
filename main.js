781b195991667515a5160d6c41bd67cad3b11311
whiterabbit09 <79956778+whiterabbit09@users.noreply.github.com>
2021-11-11 12:49:03 +0900

    11주차_과제
    과제

--- main.js
+++ main.js
@@ -24,18 +24,32 @@ navbarMenu.addEventListener("click", (event) => {
     scrollTo.scrollIntoView({behavior : 'smooth'});
 });
 
-//Contact Me 버튼 클릭시 해당 부분으로 이동
+//Contact Me 버튼 클릭시 최하단으로 이동
 const contactMe = document.querySelector(".home__contact");
 contactMe.addEventListener("click", () => {
     document.querySelector("#contact").scrollIntoView({behavior : 'smooth'});
 });
 
+//arrow-up 버튼 클릭시 최상단으로 이동
+const arrowUp = document.querySelector(".arrow-up");
+arrowUp.addEventListener("click", () => {
+    document.querySelector("#home").scrollIntoView({behavior : 'smooth'});
+});
+
+//화면을 어느 정도 내리면 home의 요소들의 투명도가 줄어듬
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
     home.style.opacity = 1 - scrollY / homeHeight;
+    if(window.scrollY > homeHeight) {
+        arrowUp.style.opacity = 1;
+    }
+    else {
+        arrowUp.style.opacity = 0;
+    }
 });
 
+//화면이 작아졌을 때 navbar의 햄버거 아이콘을 클릭하면 메뉴가 나타남
 const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
 navbarToggleBtn.addEventListener("click", () => {
     navbarMenu.classList.toggle("open");
